15da5fa12fc20021cdb695f6ef5e8d7ad3fa34c7
CoVoid <stephen@sjweiter.net>
2017-10-17 13:35:32 -0400

    Formatting

--- src/Controller.js
+++ src/Controller.js
@@ -1,12 +1,12 @@
 //Controller.js
 function update(){
-  for (x = 0; x < worldBlocks.length; x++){
-    for (y = 0; y < worldBlocks[x].length; y++){
-      for (z = 0; z < worldBlocks[x][y].length; z++){
-        console.log(worldBlocks[x][y][z]);
-      }
+    for (x = 0; x < worldBlocks.length; x++){
+        for (y = 0; y < worldBlocks[x].length; y++){
+            for (z = 0; z < worldBlocks[x][y].length; z++){
+                console.log(worldBlocks[x][y][z]);
+            }
+        }
     }
-  }
 }
 
 
--- src/Model.js
+++ src/Model.js
@@ -2,11 +2,11 @@
 var worldBlocks = Array(8);
 
 for (x = 0; x < worldBlocks.length; x++){
-  worldBlocks[x] = Array(8);
-  for (y = 0; y < worldBlocks[x].length; y++){
-    worldBlocks[x][y] = Array(8);
-    for (z = 0; z < worldBlocks[x][y].length; z++){
-      worldBlocks[x][y][z] = false;
+    worldBlocks[x] = Array(8);
+    for (y = 0; y < worldBlocks[x].length; y++){
+        worldBlocks[x][y] = Array(8);
+        for (z = 0; z < worldBlocks[x][y].length; z++){
+            worldBlocks[x][y][z] = false;
+        }
     }
-  }
 }
