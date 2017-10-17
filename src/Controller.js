1e0811d775cfe5d4ecf0a12d5702bf2f1edc17bf
CoVoid <stephen@sjweiter.net>
2017-10-17 12:27:01 -0400

    Game Loop Test Init
    -A box that floats back and forth.
    
    Issue with requestAnimationFrame() "Not defined"

--- src/Controller.js
+++ src/Controller.js
@@ -1,1 +1,24 @@
 //Controller.js
+var box = document.getElementById('box'),
+    boxPos = 10,
+    boxVelocity = 2,
+    limit = 300;
+
+var requestAnimationFrame = window.requestAnimationFrame;
+
+function draw() {
+  box.style.left = boxPos +'px';
+}
+
+function update(){
+  boxPos += boxVelocity;
+  if(boxPos >= limit || boxPos <= 0) boxVelocity = -boxVelocity;
+}
+
+function mainLoop() {
+  update();
+  draw();
+  requstAnimationFrame(mainLoop);
+}
+
+mainLoop();
